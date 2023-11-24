7eae3662497dd3deecfe63ad5d67a7878b82a3aa
admin <phentalex@gmail.com>
2023-11-24 05:23:34 +0300

    Добавьте файлы проекта.

---
+++ PreformWPF/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+using System.Configuration;
+
+namespace PreformWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NpgsqlConnection con = new NpgsqlConnection();
+        NpgsqlCommand com = new NpgsqlCommand();
+        NpgsqlDataReader dr;
+        public MainWindow()
+        {
+            InitializeComponent();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            if (VerifyUser(txtUsername.Text, txtPassword.Password))
+            {
+                MessageBox.Show("Вы успешно вошли!", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Логин или пароль введены неверно!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private bool VerifyUser(string username, string password)
+        {
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select status from users where user_login='" + username + "' and user_pass='" + password + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                if (Convert.ToBoolean(dr["status"]) == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
